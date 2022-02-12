95baf4765fddbb2fd1dd6e3df5fedf285d3cd509
heshan umayanga <heshanu97@gmail.com>
2022-02-12 20:09:00 +0530

    added validators to card form compoents.ts

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl } from '@angular/forms';
+import { FormGroup,FormControl,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-card-form',
@@ -9,7 +9,10 @@ import { FormGroup,FormControl } from '@angular/forms';
 export class CardFormComponent implements OnInit {
 
   cardForm=new FormGroup({
-    name:new FormControl('')
+    name:new FormControl('',[
+      Validators.required,
+      Validators.minLength(3)
+      ])
   });
 
   // a=1;
