e61255eac2ba8a9062dcdffe2d1e272d73d27759
heshan umayanga <heshanu97@gmail.com>
2022-02-15 23:50:29 +0530

    Added more validation in card form component.html

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -9,9 +9,9 @@ import { FormGroup,FormControl,Validators,FormBuilder} from '@angular/forms';
 export class CardFormComponent implements OnInit {
  cardForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    cardNumber:new FormControl('',[Validators.required,Validators.minLength(8)]),
-    expiration:new FormControl(''),
-    securityCode:new FormControl('',[Validators.required,Validators.minLength(3)])  
+    cardNumber:new FormControl('',[Validators.required,Validators.minLength(16),Validators.maxLength(16)]),
+    expiration:new FormControl('',[Validators.required,Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)]),
+    securityCode:new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(3)])  
   });
 
   constructor() {}
