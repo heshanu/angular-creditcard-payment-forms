9599268bf25b2a8beb2a1d2e4d2a84235757c750
heshan umayanga <heshanu97@gmail.com>
2022-02-16 00:20:24 +0530

    added validation not completed

--- src/app/date-form-control.ts
+++ src/app/date-form-control.ts
@@ -1,2 +1,11 @@
-export class DateFormControl {
+import { FormControl } from "@angular/forms";
+export class DateFormControl extends FormControl {
+    override setValue(value: string, options:any){ 
+       // console.log(value);
+       if(value.length===2){
+        super.setValue(value + '/',{...options,emitModelToViewChange:true}) 
+        return;   
+       } 
+       super.setValue(value + '*',{...options,emitModelToViewChange:true})
+    }
 }
